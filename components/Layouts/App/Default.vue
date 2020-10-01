61bf653c3fa664d56b4cef5eecd7a246adb438cf
Johnathan Barrett <johnathan.barrett@gmail.com>
2020-10-01 23:07:26 +0100

    Added functionality to add modals and made fontawesome ready to use

--- components/Layouts/App/Default.vue
+++ components/Layouts/App/Default.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <portal-target
+      v-if="showModal"
+      name="modal"
+    />
     <header class="bg-white shadow">
       <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
         <h2 class="text-3xl font-bold leading-tight text-gray-900">
@@ -16,3 +20,26 @@
     </main>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      showModal: false,
+    };
+  },
+  mounted() {
+    this.$nuxt.$on('modal:show', () => {
+      this.$nuxt.$emit('modal:showing');
+      this.showModal = true;
+      this.$nuxt.$emit('modal:shown');
+    });
+
+    this.$nuxt.$on('modal:hide', () => {
+      this.$nuxt.$emit('modal:hiding');
+      this.showModal = false;
+      this.$nuxt.$emit('modal:hidden');
+    });
+  },
+};
+</script>
